ded13285c78e8384f80b8d55d297efaa016af2b0
sovisrushain <sovisrushain@gmail.com>
2022-02-28 18:20:15 +0530

    Implement the UI

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+function Form(){
+    return(
+        <div>
+            <form>
+                <input type='text' required id='todo-item'/>
+                <button>Add To List</button>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
